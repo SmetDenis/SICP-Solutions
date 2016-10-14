7076918562a7aa61b668ab0a8aa3cc7e324c64d6
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2016-10-15 02:35:01 +0700

    Update 1_1.rkt

--- src/1_1.rkt
+++ src/1_1.rkt
@@ -1,22 +1,29 @@
 ;; ex 1.1
+
 (define a 3)
 
 (define b (+ a 1))
 
+;; 19
 (+ a b (* a b))
 
+;; #f
 (= a b)
 
+;; 3
 (if (and (> b a) (< b (* a b)))
     a
     b)
-    
+
+;; 16
 (cond ((= a 4) 6)
       ((= b 4) (+ 6 7 a))
       (else 25))
-      
+
+;; 6
 (+ 2 (if (> b a) b a))
 
+;; 16
 (* (cond ((> a b) a)
          ((< a b) b)
          (else -1))
